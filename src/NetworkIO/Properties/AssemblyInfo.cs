dcd49c47f7161a52f6248aabda9cb4a6ae59349d
gavin.wu <gavinwuz@gmail.com>
2016-04-12 14:19:32 +0800

    add project

---
+++ src/NetworkIO/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NetworkIO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NetworkIO")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("0a67163d-6c9f-431f-b9ea-8d498e7087dc")]
+
+[assembly: AssemblyVersion("1.0.1.0")]
